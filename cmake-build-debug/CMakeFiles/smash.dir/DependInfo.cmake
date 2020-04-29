a39286fb315d870f39eac65009d283c13f59bc32
guyelf@gmail.com <Guygtx4114>
2020-04-29 19:30:07 +0000

    committing all the CMake shit

---
+++ cmake-build-debug/CMakeFiles/smash.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/student/CLionProjects/Smash/ChangeDirCommand.cpp" "/home/student/CLionProjects/Smash/cmake-build-debug/CMakeFiles/smash.dir/ChangeDirCommand.cpp.o"
+  "/home/student/CLionProjects/Smash/Commands.cpp" "/home/student/CLionProjects/Smash/cmake-build-debug/CMakeFiles/smash.dir/Commands.cpp.o"
+  "/home/student/CLionProjects/Smash/GetCurrDirCommand.cpp" "/home/student/CLionProjects/Smash/cmake-build-debug/CMakeFiles/smash.dir/GetCurrDirCommand.cpp.o"
+  "/home/student/CLionProjects/Smash/JobsCommand.cpp" "/home/student/CLionProjects/Smash/cmake-build-debug/CMakeFiles/smash.dir/JobsCommand.cpp.o"
+  "/home/student/CLionProjects/Smash/PipeCommand.cpp" "/home/student/CLionProjects/Smash/cmake-build-debug/CMakeFiles/smash.dir/PipeCommand.cpp.o"
+  "/home/student/CLionProjects/Smash/RedirectionCommand.cpp" "/home/student/CLionProjects/Smash/cmake-build-debug/CMakeFiles/smash.dir/RedirectionCommand.cpp.o"
+  "/home/student/CLionProjects/Smash/ShowPidCommand.cpp" "/home/student/CLionProjects/Smash/cmake-build-debug/CMakeFiles/smash.dir/ShowPidCommand.cpp.o"
+  "/home/student/CLionProjects/Smash/Wrappers.cpp" "/home/student/CLionProjects/Smash/cmake-build-debug/CMakeFiles/smash.dir/Wrappers.cpp.o"
+  "/home/student/CLionProjects/Smash/signals.cpp" "/home/student/CLionProjects/Smash/cmake-build-debug/CMakeFiles/smash.dir/signals.cpp.o"
+  "/home/student/CLionProjects/Smash/smash.cpp" "/home/student/CLionProjects/Smash/cmake-build-debug/CMakeFiles/smash.dir/smash.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/smash.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smash.dir/ChangeDirCommand.cpp.o"
+  "CMakeFiles/smash.dir/Commands.cpp.o"
+  "CMakeFiles/smash.dir/GetCurrDirCommand.cpp.o"
+  "CMakeFiles/smash.dir/JobsCommand.cpp.o"
+  "CMakeFiles/smash.dir/PipeCommand.cpp.o"
+  "CMakeFiles/smash.dir/RedirectionCommand.cpp.o"
+  "CMakeFiles/smash.dir/ShowPidCommand.cpp.o"
+  "CMakeFiles/smash.dir/Wrappers.cpp.o"
+  "CMakeFiles/smash.dir/signals.cpp.o"
+  "CMakeFiles/smash.dir/smash.cpp.o"
+  "smash"
+  "smash.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/smash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
