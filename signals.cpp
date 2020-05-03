50d789d5430153e1993ca45a30ab9fc680f770ff
carmel1235@gmail.com <gALA098!>
2020-05-03 16:22:16 +0000

    THINGS ACTUALLY WORK OMG! signals3

--- signals.cpp
+++ signals.cpp
@@ -27,16 +27,27 @@ void ctrlZHandler(int sig_num) {
 }
 
 void ctrlCHandler(int sig_num) {
+    SmallShell &smash = SmallShell::getInstance();
     cout << "smash: got ctrl-C" << endl;
-    string res = "smash: process";
-    pid_t pid_proccess = ::getpid();
-    SmallShell& smash = SmallShell::getInstance();
-    if (smash.pid != pid_proccess){
-        kill(pid_proccess,SIGKILL);
-        smash.killProcess(pid_proccess);
-        res = res + to_string(pid_proccess) + "was killed";
-        cout<< res << endl;
+    pid_t pid = smash.fg_job->getpid();
+    if (pid) {
+        string res = "smash: process";
+        kill(pid, SIGKILL);
+        if (smash.isJobInList(pid)) {
+            smash.killProcess(pid);
+        }
+        res = res + to_string(pid) + "was killed";
+        cout << res << endl;
     }
+}
+
+
+
+
+
+
+
+
 }
 
 void alarmHandler(int sig_num) {
